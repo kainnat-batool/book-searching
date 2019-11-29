7cff95269d49948c26ff7dac0f3addbeee48d561
hafsa670 <57997249+hafsa670@users.noreply.github.com>
2019-11-30 00:20:13 +0500

    Add files via upload

---
+++ booki.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>book searching</title>
+		<link rel="stylesheet" href="style11.css">
+		<body>
+			<div class="login-box">
+				<h1>Book Searching</h1>
+				<div class="textbox">
+<i class="fa fa-book" aria-hidden="true"></i>					
+<input type="text" placeholder="booktitle" name="" value="">
+				</div>
+				<input class="btn" type="button" OnClick=location.href="result.php" name="" value="Search">
+			</div>
+		</body>
+	</head>
+</html>
