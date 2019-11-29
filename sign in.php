b5b23507053df97abf858f8d19bdf69775bb6ed0
hafsa670 <57997249+hafsa670@users.noreply.github.com>
2019-11-30 00:21:03 +0500

    Add files via upload

---
+++ sign in.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Sign in Form</title>
+		<link rel="stylesheet" href="style11.css">
+		<body>
+			<div class="login-box">
+				<h1>Sign up</h1>
+				<div class="textbox">
+					<i class="fa fa-user" aria-hidden="true"></i>
+					<input type="text" placeholder="username" name="" value="">
+				</div>
+				<div class="textbox">
+					<i class="fa fa-lock" aria-hidden="true"></i>
+					<input type="password" placeholder="passsword" name="" value="">
+				</div>
+				<div class="textbox">
+					<i class="fa fa-envelope" aria-hidden="true"></i>
+					<input type="email" placeholder="email" name="" value="">
+				</div>
+				<div class="textbox">
+
+					<p>GENDER</p>
+					<select name="gender">
+                          <option value="MALE">
+                                  Male
+                                  </option>
+                       <option value="FEMALE">
+                             Female
+                               </option>
+                        <option value="OTHER">
+                              other
+                              </option>
+                                </select>
+				</div>
+				 
+			<input class="btn" type="button" OnClick=location.href="aoaloginform.php" name="" value="Sign-in">
+</div>
+		</body>
+	</head>
+</html>
