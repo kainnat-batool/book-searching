61a4e9279d346d0c162415a0612b9011082a4edc
hafsa670 <57997249+hafsa670@users.noreply.github.com>
2019-11-30 00:20:46 +0500

    Add files via upload

---
+++ result.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Result Form</title>
+		<link rel="stylesheet" href="style11.css">
+		<body>
+			<div class="login-box">
+				<h1>Result</h1>
+				<div class="textbox">
+					<i class="fa fa-book" aria-hidden="true"></i>					
+                     <input type="text" placeholder="booktitle" name="" value="">
+				</div>
+				<div class="textbox">
+					<i class="fa fa-user" aria-hidden="true"></i>
+					<input type="text" placeholder="bookauthor" name="" value="">
+				</div>
+					<div class="textbox">
+					<i class="fa fa-chevron-right" aria-hidden="true"></i>
+					<input type="text" placeholder="rackno" name="" value="">
+				</div>
+					<div class="textbox">
+					<i class="fa fa-file-text" aria-hidden="true"></i>
+					<input type="text" placeholder="pages" name="" value="">
+				</div>
+				<input class="btn" type="button" name="" value="OK">
+			</div>
+		</body>
+	</head>
+</html>
