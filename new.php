46639d16ae1a2867471de8943007efd7e38de68e
hafsa670 <57997249+hafsa670@users.noreply.github.com>
2019-12-08 12:32:26 +0500

    Add files via upload

--- new.php
+++ new.php
@@ -5,17 +5,17 @@ $a = 0;
 $dbHost = 'localhost';
 $dbUsername = 'root';
 $dbPassword = '';
-$dbName = 'db';
+$dbName = 'bookdata';
 //Create connection and select DB
 $conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 if ($conn->connect_error) {
     die("Unable to connect database: " . $db->connect_error);
 }
-$query = "SELECT * FROM db";
+$query = "SELECT * FROM bookdt";
 $result = mysqli_query($conn, $query);
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
-        $datas[] = $row['title'];
+        $datas[] = $row['booktitle'];
     }
     // echo "data stored\n";
 }
@@ -53,15 +53,9 @@ if (isset($_POST['SEARCH'])) {
         return false;
     }
     if (binarySearch($datas, $se) == true) {
-        echo $se . " Exists";?> <br> <?php
-        $query = "SELECT * FROM db where title = '$se' ";
-        $result = mysqli_query($conn, $query);
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                print_r($row);
-            }
-        }
-    } else {
+        echo $se . " Exists";
+    }
+      else {
         echo $se . " Doesnt Exist";
     }
 }
@@ -71,14 +65,47 @@ if (isset($_POST['SEARCH'])) {
 <html>
 
 <head>
+    <meta charset="utf-8">
     <title>SEARCH PAGE</title>
-</head>
+    <link rel="stylesheet" href="style11.css">
 
-<body>
+<body text="white">
+    <div class="login-box">
+    <h1>Book Searching</h1>
     <form action="" method="POST">
+        <div class="textbox">
+            <i class="fa fa-book" aria-hidden="true"></i>   
         Booktitle:<input type="text" name="booktitle"><br><br>
-        <input type="submit" name="SEARCH" value="SEARCH"><br><br>
+        </div>
+        <input class="btn" type="submit" name="SEARCH" value="SEARCH"><br><br>
+        <table align="left" border="1px" style="width=50%;">
+            <tr>
+            <th>Id</th>
+            <th>rackno</th>
+            <th>booktitle</th>
+            <th>bookauthor</th>
+            <th>pages</th>
+        </tr>
+        <?php
+           $query = "SELECT * FROM bookdt where booktitle = '$se' ";
+        $result = mysqli_query($conn, $query);
+        if (mysqli_num_rows($result) > 0) {
+            while ($rows = mysqli_fetch_assoc($result)) {
+               ?>
+            <tr>
+            <td><?php echo $rows['id']?></td>
+            <td><?php echo $rows['rackno']?></td>
+            <td><?php echo $rows['booktitle']?></td>
+            <td><?php echo $rows['bookauthor']?></td>
+            <td><?php echo $rows['pages']?></td>
+            </tr>
+            <?php
+            }
+        }
+        ?>
+    </table>
+    </div>
     </form>
 </body>
-
+</head>
 </html>
